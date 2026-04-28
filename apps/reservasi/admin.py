2aa40bada14e996991f4fa9f08b32e0a9b62e77c
syayid <syayidsyayid262@gmail.com>
2026-04-28 20:03:49 +0700

    Menambahkan model registration

--- apps/reservasi/admin.py
+++ apps/reservasi/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Lapangan, JadwalOperasional, Booking, Pembayaran, LogStatus
 
 # Register your models here.
+admin.site.register(Lapangan)
+admin.site.register(JadwalOperasional)
+admin.site.register(Booking)
+admin.site.register(Pembayaran)
+admin.site.register(LogStatus)
