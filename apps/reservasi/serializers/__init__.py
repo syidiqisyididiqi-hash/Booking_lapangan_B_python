fa6d62fdbb8aa337ba1318644767652945b294bf
syayid <syayidsyayid262@gmail.com>
2026-04-30 10:19:40 +0700

    create booking serializers

--- apps/reservasi/serializers/__init__.py
+++ apps/reservasi/serializers/__init__.py
@@ -2,9 +2,12 @@ from .lapangan_serializer import LapanganSerializer
 from .jadwal_operasional_serializer import JadwaloperasionalSerializer
 from .log_status_serializer import LogStatusSerializer
 from .pembayaran_serializer import PembayaranSerializer
+from .booking_serializer import BookingSerializer
+
 __all__ = [
     'LapanganSerializer',
     'JadwaloperasionalSerializer',
     'LogStatusSerializer',
     'PembayaranSerializer',
+    'BookingSerializer',
 ]
---
+++ apps/reservasi/serializers/booking_serializer.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+# from django.utils import timezone
+from ..models import Booking
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+    def validate(self, data):
+        lapangan = data['lapangan']
+        tanggal = data['tanggal']
+        jam_mulai = data['jam_mulai']
+        jam_selesai = data['jam_selesai']
+
+        if jam_mulai >= jam_selesai:
+            raise serializers.ValidationError("Jam selesai harus lebih besar dari jam mulai.")
+        
+        bentrok = Booking.objects.filter(
+            lapangan=lapangan,
+            tanggal=tanggal,
+            jam_mulai__lt=jam_selesai,
+            jam_selesai__gt=jam_mulai,
+            status_booking__in=['menunggu', 'doterima']
+        ).exists()
+
+        if bentrok:
+            raise serializers.ValidationError("Jadwal bentrok dengan booking lain.")
+        
+        return data
